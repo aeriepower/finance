62af7e31b4f3901d5e89d26629aa1ec28eb8f7ff
David <hurtado.banda.david@gmail.com>
2016-04-14 23:27:10 +0200

    Moved highlight to external layout

---
+++ resources/views/layouts/highlight.blade.php
@@ -0,0 +1,19 @@
+<div class="col-xs-12 col-md-6 col-lg-3">
+    <div class="panel panel-blue panel-widget ">
+        <div class="row no-padding">
+            <div class="col-sm-3 col-lg-5 widget-left">
+                <svg class="glyph stroked bag"><use xlink:href="#stroked-bag"></use></svg>
+            </div>
+            <div class="col-sm-9 col-lg-7 widget-right">
+                <div class="large">120</div>
+                <div class="text-muted">New Orders</div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--
+panel-blue
+panel-teal
+panel-orange
+panel-red
+-->
