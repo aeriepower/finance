bf28a8fe00e28f650e47e7891f62e9320e6e2053
David <hurtado.banda.david@gmail.com>
2016-04-15 00:06:01 +0200

    Moved all the main layout "index content" to index blade and secondary layouts to "elements" folder

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,21 +1,3 @@
-<!--doctype html>
-<!html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Interviews</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-    @yield('style')
-</head>
-<body>
-    @yield('content')
-
-    @yield('script')
-</body>
-</html-->
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -44,58 +26,9 @@
 
 <div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
 
-    @include('layouts.breadcrumb')
+    @yield('content')
 
-    @include('layouts.title')
-
-
-    <div class="row">
-        @include('layouts.highlight')
-
-        @include('layouts.highlight')
-
-        @include('layouts.highlight')
-
-        @include('layouts.highlight')
-    </div><!--/.row-->
-
-    <div class="row">
-        <div class="col-lg-12">
-            <div class="panel panel-default">
-                <div class="panel-heading">Site Traffic Overview</div>
-                <div class="panel-body">
-                    <div class="canvas-wrapper">
-                        <canvas class="main-chart" id="line-chart" height="200" width="600"></canvas>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div><!--/.row-->
-
-    <div class="row">
-        @include('layouts.roundchart')
-
-        @include('layouts.roundchart')
-
-        @include('layouts.roundchart')
-
-        @include('layouts.roundchart')
-    </div><!--/.row-->
-
-    <div class="row">
-        <div class="col-md-8">
-
-            @include('layouts.chat')
-
-        </div><!--/.col-->
-
-        <div class="col-md-4">
-
-            @include('layouts.todolist')
-
-        </div><!--/.col-->
-    </div><!--/.row-->
-</div>	<!--/.main-->
+</div>
 
 <script src="js/jquery-1.11.1.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
@@ -105,11 +38,10 @@
 <script src="js/easypiechart-data.js"></script>
 <script src="js/bootstrap-datepicker.js"></script>
 <script>
-    $('#calendar').datepicker({
-    });
+    $('#calendar').datepicker({});
 
     !function ($) {
-        $(document).on("click","ul.nav li.parent > a > span.icon", function(){
+        $(document).on("click", "ul.nav li.parent > a > span.icon", function () {
             $(this).find('em:first').toggleClass("glyphicon-minus");
         });
         $(".sidebar span.icon").find('em:first').addClass("glyphicon-plus");
--- resources/views/report/index.blade.php
+++ resources/views/report/index.blade.php
@@ -2,4 +2,53 @@
 
 @section('content')
 
+    @include('layouts.elements.breadcrumb')
+
+    @include('layouts.elements.title')
+    <div class="row">
+        @include('layouts.elements.highlight')
+
+        @include('layouts.elements.highlight')
+
+        @include('layouts.elements.highlight')
+
+        @include('layouts.elements.highlight')
+    </div><!--/.row-->
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">Site Traffic Overview</div>
+                <div class="panel-body">
+                    <div class="canvas-wrapper">
+                        <canvas class="main-chart" id="line-chart" height="200" width="600"></canvas>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div><!--/.row-->
+
+    <div class="row">
+        @include('layouts.elements.roundchart')
+
+        @include('layouts.elements.roundchart')
+
+        @include('layouts.elements.roundchart')
+
+        @include('layouts.elements.roundchart')
+    </div><!--/.row-->
+
+    <div class="row">
+        <div class="col-md-8">
+
+            @include('layouts.elements.chat')
+
+        </div><!--/.col-->
+
+        <div class="col-md-4">
+
+            @include('layouts.elements.todolist')
+
+        </div><!--/.col-->
+    </div><!--/.row-->
+
 @stop
