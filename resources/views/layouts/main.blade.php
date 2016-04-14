c53b33f16e1dc23c9ce643157e131cb8ba0affd5
David <hurtado.banda.david@gmail.com>
2016-04-14 23:10:00 +0200

    Moved breadcrumb to external layout

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -43,18 +43,8 @@
 @include('layouts.sidebar')
 
 <div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
-    <div class="row">
-        <ol class="breadcrumb">
-            <li><a href="#"><svg class="glyph stroked home"><use xlink:href="#stroked-home"></use></svg></a></li>
-            <li class="active">Icons</li>
-        </ol>
-    </div><!--/.row-->
 
-    <div class="row">
-        <div class="col-lg-12">
-            <h1 class="page-header">Dashboard</h1>
-        </div>
-    </div><!--/.row-->
+    @include('layouts.breadcrumb')
 
     <div class="row">
         <div class="col-xs-12 col-md-6 col-lg-3">
