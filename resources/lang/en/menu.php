a6d1256bdecdfb99d17085012c2cacc062342f48
David <hurtado.banda.david@gmail.com>
2016-04-15 00:15:16 +0200

    Added en language translate for the sidebar menu

---
+++ resources/lang/en/menu.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'dashboard' => 'Dashboard',
+    'widgets' => 'Widgets',
+    'charts' => 'Charts',
+    'tables' => 'Tables',
+    'forms' => 'Forms',
+    'alerts' => 'Alerts',
+    'icons' => 'Icons',
+    'dropdown' => 'Dropdown',
+    'login' => 'Login',
+    'subitem' => 'Subitem',
+    'search' => 'Search'
+];
