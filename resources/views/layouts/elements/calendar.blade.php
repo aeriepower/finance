cad2754c7cb91e27b92ed34e7180561209f5773a
David <hurtado.banda.david@gmail.com>
2016-04-15 00:24:03 +0200

    Added calendar widget

---
+++ resources/views/layouts/elements/calendar.blade.php
@@ -0,0 +1,6 @@
+<div class="panel panel-red">
+    <div class="panel-heading dark-overlay"><svg class="glyph stroked calendar"><use xlink:href="#stroked-calendar"></use></svg>Calendar</div>
+    <div class="panel-body">
+        <div id="calendar"></div>
+    </div>
+</div>
