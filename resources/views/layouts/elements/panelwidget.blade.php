7a3097b1a7b0d3e48de4defd2e44f24c8bb54530
David Hurtado <david.hurtado@rkpeople.com>
2016-04-15 13:20:12 +0200

    Added widget panel layout

---
+++ resources/views/layouts/elements/panelwidget.blade.php
@@ -0,0 +1,12 @@
+<div class="panel panel-blue">
+    <div class="panel-heading dark-overlay">Blue Panel</div>
+    <div class="panel-body">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ut ante in sapien blandit luctus sed ut lacus. Phasellus urna est, faucibus nec ultrices placerat, feugiat et ligula. Donec vestibulum magna a dui pharetra molestie. Fusce et dui urna.</p>
+    </div>
+</div>
+{{--
+panel-blue
+panel-teal
+panel-orange
+panel-red
+--}}
