1d13db31e8a012e25045f2c3767d3b5f9dc7056b
David Hurtado <david.hurtado@rkpeople.com>
2016-04-15 13:16:42 +0200

    Added basic panel layout

---
+++ resources/views/layouts/elements/basicpanel.blade.php
@@ -0,0 +1,16 @@
+<div class="panel panel-default">
+    <div class="panel-heading">
+        Default Panel
+    </div>
+    <div class="panel-body">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ut ante in sapien blandit luctus sed ut lacus. Phasellus urna est, faucibus nec ultrices placerat, feugiat et ligula. Donec vestibulum magna a dui pharetra molestie. Fusce et dui urna.</p>
+    </div>
+</div>
+{{--
+panel-default
+panel-primary
+panel-success
+panel-info
+panel-warning
+panel-danger
+--}}
