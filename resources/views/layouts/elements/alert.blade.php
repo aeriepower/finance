bb9ed94b2f6dd8929e6ca881479a0289cbf29b31
David Hurtado <david.hurtado@rkpeople.com>
2016-04-15 13:13:57 +0200

    Added alert layout

---
+++ resources/views/layouts/elements/alert.blade.php
@@ -0,0 +1,9 @@
+<div class="alert bg-primary" role="alert">
+    <svg class="glyph stroked empty-message"><use xlink:href="#stroked-empty-message"></use></svg> Welcome to the admin dashboard panel bootstrap template <a href="#" class="pull-right"><span class="glyphicon glyphicon-remove"></span></a>
+</div>
+{{--
+bg-primary => gris
+bg-success => verde
+bg-warning => amarillo
+bg-danger => rojo
+--}}
