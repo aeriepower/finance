99f7ae4914b62aa9c749c97653983880073393ad
David Hurtado <david.hurtado@rkpeople.com>
2016-04-15 13:21:21 +0200

    Added tabs panel layout

---
+++ resources/views/layouts/elements/tab.blade.php
@@ -0,0 +1,29 @@
+<div class="panel-body tabs">
+
+    <ul class="nav nav-tabs">
+        <li class="active"><a href="#tab1" data-toggle="tab">Tab 1</a></li>
+        <li><a href="#tab2" data-toggle="tab">Tab 2</a></li>
+        <li><a href="#tab3" data-toggle="tab">Tab 3</a></li>
+    </ul>
+
+    <div class="tab-content">
+        <div class="tab-pane fade in active" id="tab1">
+            <h4>Tab 1</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget rutrum purus. Donec hendrerit ante ac metus sagittis elementum. Mauris feugiat nisl sit amet neque luctus, a tincidunt odio auctor. </p>
+        </div>
+        <div class="tab-pane fade" id="tab2">
+            <h4>Tab 2</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget rutrum purus. Donec hendrerit ante ac metus sagittis elementum. Mauris feugiat nisl sit amet neque luctus, a tincidunt odio auctor. </p>
+        </div>
+        <div class="tab-pane fade" id="tab3">
+            <h4>Tab 3</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget rutrum purus. Donec hendrerit ante ac metus sagittis elementum. Mauris feugiat nisl sit amet neque luctus, a tincidunt odio auctor. </p>
+        </div>
+    </div>
+</div>
+</div><!--/.panel-->
+
+{{--
+nav-tabs
+nav-pills
+--}}
