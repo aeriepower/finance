75656334837e5336f7f86f28a93adfa79324d37d
David <hurtado.banda.david@gmail.com>
2016-04-15 18:16:31 +0200

    Capitalized first letter on sidebar menu

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -2,7 +2,7 @@
 <div id="sidebar-collapse" class="col-sm-3 col-lg-2 sidebar">
     <form role="search">
         <div class="form-group">
-            <input type="text" class="form-control" placeholder="{{ trans('menu.search') }}">
+            <input type="text" class="form-control" placeholder="{{ trans('topbar.search') }}">
         </div>
     </form>
     <ul class="nav menu">
@@ -10,7 +10,7 @@
             $sidebarElements = trans('sidebar');
         ?>
         @foreach ($sidebarElements as $element)
-            <li><a href="widgets.html"><svg class="glyph stroked calendar"><use xlink:href="#stroked-calendar"></use></svg> {{ $element }}</a></li>
+            <li><a href="widgets.html"><svg class="glyph stroked calendar"><use xlink:href="#stroked-calendar"></use></svg> {{ ucfirst($element) }}</a></li>
         @endforeach
         {{--
         <li class="parent ">
