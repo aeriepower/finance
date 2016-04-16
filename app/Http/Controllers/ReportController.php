885ebf8d853c98a7253e4f272856bc72f52cae15
David <hurtado.banda.david@gmail.com>
2016-04-16 20:53:05 +0200

    Added middleware auth in ReportController and TransactionController

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -8,6 +8,17 @@ use Finance\Http\Requests;
 
 class ReportController extends Controller
 {
+    /**
+     * ReportController constructor.
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function index()
     {
         return view('report.index',[
--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -10,6 +10,16 @@ use Illuminate\Support\Facades\Auth;
 
 class TransactionController extends Controller
 {
+
+    /**
+     * TransactionController constructor.
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+
     /**
      * Display a listing of the resource.
      *
