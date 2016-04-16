1b63dec15e8da16808225d83db3f3570ed3857cf
David <hurtado.banda.david@gmail.com>
2016-04-16 19:27:23 +0200

    Added error msg in case there are

--- resources/views/layouts/elements/alert.blade.php
+++ resources/views/layouts/elements/alert.blade.php
@@ -1,5 +1,7 @@
-<div class="alert bg-primary" role="alert">
-    <svg class="glyph stroked empty-message"><use xlink:href="#stroked-empty-message"></use></svg> Welcome to the admin dashboard panel bootstrap template <a href="#" class="pull-right"><span class="glyphicon glyphicon-remove"></span></a>
+<div class="alert bg-danger" role="alert">
+    <svg class="glyph stroked empty-message"><use xlink:href="#stroked-empty-message"></use></svg>
+        {{ $msg }}
+    <a href="#" class="pull-right"><span class="glyphicon glyphicon-remove"></span></a>
 </div>
 {{--
 bg-primary => gris
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -28,6 +28,12 @@
 
 <div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
 
+    @if(count($errors) > 0)
+        @foreach($errors->all() as $msg)
+            @include('layouts.elements.alert')
+        @endforeach
+    @endif
+
     @yield('content')
 
 </div>
