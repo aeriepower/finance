770ecd913e1305a51237962f0ac072e6725bf67b
David <hurtado.banda.david@gmail.com>
2016-04-16 10:35:04 +0200

    Inserted loops into advanced table to show the views

--- resources/views/layouts/elements/advancedtable.blade.php
+++ resources/views/layouts/elements/advancedtable.blade.php
@@ -4,13 +4,21 @@
         <table data-toggle="table" data-url="tables/data1.json"  data-show-refresh="true" data-show-toggle="true" data-show-columns="true" data-search="true" data-select-item-name="toolbar1" data-pagination="true" data-sort-name="name" data-sort-order="desc">
             <thead>
             <tr>
-                <th data-field="state" data-checkbox="true" >Item ID</th>
-                <th data-field="id" data-sortable="true">Item ID</th>
-                <th data-field="name"  data-sortable="true">Item Name</th>
-                <th data-field="price" data-sortable="true">Item Price</th>
+                @if(!empty($tableData))
+                    @foreach($tableData[0]->getAttributes() as $key => $value)
+                        <th>{{ $key }}</th>
+                    @endforeach
+                @endif
             </tr>
             </thead>
             <tbody>
+            @foreach($tableData as $element)
+            <tr>
+                @foreach($element->getAttributes() as $key => $value)
+                    <td>{{ $value }}</td>
+                @endforeach
+            </tr>
+            @endforeach
             </tbody>
         </table>
     </div>
