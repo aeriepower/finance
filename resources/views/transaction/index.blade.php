65455b0ae3ba85cf225f6d5a8f42aff1d521000c
David <hurtado.banda.david@gmail.com>
2016-04-16 10:36:07 +0200

    Removed unnecesary script

--- resources/views/transaction/index.blade.php
+++ resources/views/transaction/index.blade.php
@@ -13,42 +13,10 @@
         </div>
     </div>
 
-    <script>
-        !function ($) {
-            $(document).on("click", "ul.nav li.parent > a > span.icon", function () {
-                $(this).find('em:first').toggleClass("glyphicon-minus");
-            });
-            $(".sidebar span.icon").find('em:first').addClass("glyphicon-plus");
-        }(window.jQuery);
-
-        $(window).on('resize', function () {
-            if ($(window).width() > 768) $('#sidebar-collapse').collapse('show')
-        })
-        $(window).on('resize', function () {
-            if ($(window).width() <= 767) $('#sidebar-collapse').collapse('hide')
-        })
-    </script>
 @stop
 
 @section('script')
 
     {!! Html::script('js/bootstrap-table.js') !!}
 
-    <script>
-        $('#calendar').datepicker({});
-
-        !function ($) {
-            $(document).on("click", "ul.nav li.parent > a > span.icon", function () {
-                $(this).find('em:first').toggleClass("glyphicon-minus");
-            });
-            $(".sidebar span.icon").find('em:first').addClass("glyphicon-plus");
-        }(window.jQuery);
-
-        $(window).on('resize', function () {
-            if ($(window).width() > 768) $('#sidebar-collapse').collapse('show')
-        })
-        $(window).on('resize', function () {
-            if ($(window).width() <= 767) $('#sidebar-collapse').collapse('hide')
-        })
-    </script>
 @stop
