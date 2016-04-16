dc2f4e1142662a92caa873800733df401132a96a
David <hurtado.banda.david@gmail.com>
2016-04-16 13:02:15 +0200

    Added button to add resource to the layout

--- resources/views/layouts/elements/advancedtable.blade.php
+++ resources/views/layouts/elements/advancedtable.blade.php
@@ -1,5 +1,9 @@
 <div class="panel panel-default">
-    <div class="panel-heading">Advanced Table</div>
+    <div class="panel-heading">
+        <a type="button" class="btn btn-success">
+            <i class="glyphicon glyphicon glyphicon-plus icon-list-alt"></i> {{ trans('helper.add') . ' ' . $title }}
+        </a>
+    </div>
     <div class="panel-body">
         <table
                 data-toggle="table"
