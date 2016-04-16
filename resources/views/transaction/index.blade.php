20626ba4b1b2b4a7d40f44aac0b74dfd791be7ac
David <hurtado.banda.david@gmail.com>
2016-04-16 19:06:44 +0200

    Moved "add transaction" button out of table layout

--- resources/views/transaction/index.blade.php
+++ resources/views/transaction/index.blade.php
@@ -9,6 +9,11 @@
     @include('layouts.elements.title')
     <div class="row">
         <div class="col-lg-12">
+            <a type="button" class="btn btn-success" href="{{url($_SERVER['REQUEST_URI'] . '/create')}}">
+                <i class="glyphicon glyphicon glyphicon-plus icon-list-alt"></i> {{ ucfirst(trans('helper.add')) . ' ' . $title }}
+            </a>
+            <br>
+            <br>
             @include('layouts.elements.advancedtable')
         </div>
     </div>
