b71b851556d133e6e5601443b2ba48e64c1c10c2
David <hurtado.banda.david@gmail.com>
2016-04-16 09:18:26 +0200

    Added tables layouts

---
+++ resources/views/layouts/elements/advancedtable.blade.php
@@ -0,0 +1,15 @@
+<div class="panel panel-default">
+    <div class="panel-heading">Advanced Table</div>
+    <div class="panel-body">
+        <table data-toggle="table" data-url="tables/data1.json"  data-show-refresh="true" data-show-toggle="true" data-show-columns="true" data-search="true" data-select-item-name="toolbar1" data-pagination="true" data-sort-name="name" data-sort-order="desc">
+            <thead>
+            <tr>
+                <th data-field="state" data-checkbox="true" >Item ID</th>
+                <th data-field="id" data-sortable="true">Item ID</th>
+                <th data-field="name"  data-sortable="true">Item Name</th>
+                <th data-field="price" data-sortable="true">Item Price</th>
+            </tr>
+            </thead>
+        </table>
+    </div>
+</div>
---
+++ resources/views/layouts/elements/basictable.blade.php
@@ -0,0 +1,14 @@
+<div class="panel panel-default">
+    <div class="panel-heading">Styled Table</div>
+    <div class="panel-body">
+        <table data-toggle="table" id="table-style" data-url="tables/data2.json" data-row-style="rowStyle">
+            <thead>
+            <tr>
+                <th data-field="id" data-align="right" >Item ID</th>
+                <th data-field="name" >Item Name</th>
+                <th data-field="price" >Item Price</th>
+            </tr>
+            </thead>
+        </table>
+    </div>
+</div>
