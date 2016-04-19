b5010f8223a3accb7f212c8bb7d4d9f562051f19
David Hurtado <david.hurtado@rkpeople.com>
2016-04-19 09:56:03 +0200

    Changed top bar translations

--- resources/views/layouts/topbar.blade.php
+++ resources/views/layouts/topbar.blade.php
@@ -10,11 +10,11 @@
             <a class="navbar-brand" href="#"><span>my</span>Finance</a>
             <ul class="user-menu">
                 <li class="dropdown pull-right">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><svg class="glyph stroked male-user"><use xlink:href="#stroked-male-user"></use></svg> {!! trans('menu.user') !!} <span class="caret"></span></a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><svg class="glyph stroked male-user"><use xlink:href="#stroked-male-user"></use></svg> {!! trans('topbar.user') !!} <span class="caret"></span></a>
                     <ul class="dropdown-menu" role="menu">
-                        <li><a href="#"><svg class="glyph stroked male-user"><use xlink:href="#stroked-male-user"></use></svg> {!! trans('menu.profile') !!}</a></li>
-                        <li><a href="#"><svg class="glyph stroked gear"><use xlink:href="#stroked-gear"></use></svg> {!! trans('menu.settings') !!}</a></li>
-                        <li><a href="#"><svg class="glyph stroked cancel"><use xlink:href="#stroked-cancel"></use></svg> {!! trans('menu.logout') !!}</a></li>
+                        <li><a href="#"><svg class="glyph stroked male-user"><use xlink:href="#stroked-male-user"></use></svg> {!! trans('topbar.profile') !!}</a></li>
+                        <li><a href="#"><svg class="glyph stroked gear"><use xlink:href="#stroked-gear"></use></svg> {!! trans('topbar.settings') !!}</a></li>
+                        <li><a href="#"><svg class="glyph stroked cancel"><use xlink:href="#stroked-cancel"></use></svg> {!! trans('topbar.logout') !!}</a></li>
                     </ul>
                 </li>
             </ul>
