173e3223e9c390e3a1d6b2b0b2ea02825bfde5ea
David <hurtado.banda.david@gmail.com>
2016-04-20 08:12:49 +0200

    Removed fake data from main layout

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -44,7 +44,6 @@ ini_set('xdebug.max_nesting_level', 3000);
 {!! Html::script('js/jquery-1.11.1.min.js') !!}
 {!! Html::script('js/bootstrap.min.js') !!}
 {!! Html::script('js/chart.min.js') !!}
-{!! Html::script('js/chart-data.js') !!}
 {!! Html::script('js/easypiechart.js') !!}
 {!! Html::script('js/easypiechart-data.js') !!}
 {!! Html::script('js/bootstrap-datepicker.js') !!}
