136bbb321d810d0a62a3b3cd3550fecd144c1d02
David <hurtado.banda.david@gmail.com>
2016-04-23 12:16:02 +0200

    Reformat code at todolist

--- resources/views/layouts/elements/todolist.blade.php
+++ resources/views/layouts/elements/todolist.blade.php
@@ -1,26 +1,43 @@
 <div class="panel panel-blue">
-    <div class="panel-heading dark-overlay"><svg class="glyph stroked clipboard-with-paper"><use xlink:href="#stroked-clipboard-with-paper"></use></svg>To-do List</div>
+    <div class="panel-heading dark-overlay">
+        <svg class="glyph stroked clipboard-with-paper">
+            <use xlink:href="#stroked-clipboard-with-paper"></use>
+        </svg>
+        To-do List
+    </div>
     <div class="panel-body">
         <ul class="todo-list">
             <li class="todo-list-item">
                 <div class="checkbox">
-                    <input type="checkbox" id="checkbox" />
+                    <input type="checkbox" id="checkbox"/>
                     <label for="checkbox">Make a plan for today</label>
                 </div>
                 <div class="pull-right action-buttons">
-                    <a href="#"><svg class="glyph stroked pencil"><use xlink:href="#stroked-pencil"></use></svg></a>
-                    <a href="#" class="flag"><svg class="glyph stroked flag"><use xlink:href="#stroked-flag"></use></svg></a>
-                    <a href="#" class="trash"><svg class="glyph stroked trash"><use xlink:href="#stroked-trash"></use></svg></a>
+                    <a href="#">
+                        <svg class="glyph stroked pencil">
+                            <use xlink:href="#stroked-pencil"></use>
+                        </svg>
+                    </a>
+                    <a href="#" class="flag">
+                        <svg class="glyph stroked flag">
+                            <use xlink:href="#stroked-flag"></use>
+                        </svg>
+                    </a>
+                    <a href="#" class="trash">
+                        <svg class="glyph stroked trash">
+                            <use xlink:href="#stroked-trash"></use>
+                        </svg>
+                    </a>
                 </div>
             </li>
         </ul>
     </div>
     <div class="panel-footer">
         <div class="input-group">
-            <input id="btn-input" type="text" class="form-control input-md" placeholder="Add new task" />
-							<span class="input-group-btn">
-								<button class="btn btn-primary btn-md" id="btn-todo">Add</button>
-							</span>
+            <input id="btn-input" type="text" class="form-control input-md" placeholder="Add new task"/>
+			<span class="input-group-btn">
+				<button class="btn btn-primary btn-md" id="btn-todo">Add</button>
+			</span>
         </div>
     </div>
 </div>
